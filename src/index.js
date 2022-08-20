a82399f6dd05ae48e209308e0aee0e036dfe4094
nguyenvantoan1821051127 <79552710+nguyenvantoan1821051127@users.noreply.github.com>
2022-08-20 15:15:55 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import UseStateDemo from './pages/HookDemo/UseStateDemo/UseStateDemo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+      <Routes>
+        <Route path='' element={<App></App>}>
+          <Route path='usestate'element={<UseStateDemo></UseStateDemo>}></Route>
+        </Route>
+      </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
